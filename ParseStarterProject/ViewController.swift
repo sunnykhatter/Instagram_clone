9bc7846614d65494e5d2c3cd6600227b3b8452d4
Lakshay Khatter <lakshaykhatter@gmail.com>
2016-07-16 21:33:07 -0400

    Finished signup function, displays alert if error occurs

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -25,13 +25,69 @@ class ViewController: UIViewController {
     @IBOutlet weak var username_tf: UITextField!
     @IBOutlet weak var password_tf: UITextField!
 
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
 
-    @IBAction func signUp(sender: AnyObject) {
+    func displayAlert(title:String, message:String) {
         
+        var alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction((UIAlertAction(title: "OK", style: .Default, handler: { (action) in
+        self.dismissViewControllerAnimated(true, completion: nil) })))
+        self.presentViewController(alert, animated: true, completion: nil)
+
+    }
+    
+    
+    
+    @IBAction func signUp(sender: AnyObject) {
+        if username_tf.text == "" || password_tf.text == "" {
+            displayAlert("Error in Form", message: "Please enter a username")
+            
+        } else {
+            activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0,0,50,50))
+            activityIndicator.center = self.view.center
+            activityIndicator.hidesWhenStopped = true
+            activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+            view.addSubview(activityIndicator)
+            activityIndicator.startAnimating()
+            UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+            
+            var user = PFUser()
+            user.username = username_tf.text
+            user.password = password_tf.text
+            
+            var errorMessage = "Please try again later"
+            
+            user.signUpInBackgroundWithBlock({ (success, error) in
+                self.activityIndicator.stopAnimating()
+                UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                
+                if error == nil {
+                    // Signup Successful
+                } else {
+                    if let errorString = error!.userInfo["error"] as? NSString{
+                        
+                        errorMessage = errorString as String
+                            
+                        
+                    }
+                    self.displayAlert("Failed Signup", message: errorMessage)
+                }
+                
+            })
+            
+        }
+    
+    
+    
+    
+    
     }
 
-    @IBAction func logIn(sender: AnyObject) {
     
+        
+        
+    @IBAction func logIn(sender: AnyObject) {
+        
     }
     
 
